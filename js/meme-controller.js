c72394bef0030d2355524f65de31e783f08e71f0
Barak Levav <levavbarak@gmail.com>
2022-05-11 21:25:29 +0300

    improved game logic, one small bug remains but other than that game works fine.

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -3,209 +3,302 @@
 var gElCanvas;
 var gCtx;
 var gCurrImgUrl;
-var gCurrMemeLines;
 var gTopTxtPos;
 var gBottomTxtPos;
 var gCenterTxtPos;
 var gNumOfLines;
 var gAddedThirdLine;
 var gLastLine;
+var gHorizontalPos;
 
 function initEditor() {
-    gElCanvas = document.getElementById("my-canvas");
-    gCtx = gElCanvas.getContext("2d");
-    var elEditor = document.querySelector('.editor-container');
-    elEditor.style.display = 'flex';
-    var elGallery = document.querySelector('.image-container');
-    elGallery.style.display = 'none';
-    gCurrMemeLines = gMeme.lines[0];
-    gCurrMemeLines.txt = '';
-    gTopTxtPos = 50;
-    gBottomTxtPos = 460;
-    gCenterTxtPos = 265;
-    gNumOfLines = 1;
-    gAddedThirdLine = false;
-    gLastLine = 0;
-    renderMeme();
+  gElCanvas = document.getElementById('my-canvas');
+  gCtx = gElCanvas.getContext('2d');
+  var elEditor = document.querySelector('.editor-container');
+  elEditor.style.display = 'flex';
+  var elGallery = document.querySelector('.image-container');
+  elGallery.style.display = 'none';
+  gMeme.selectedLineIdx = 0;
+  gMeme.lines[gMeme.selectedLineIdx].txt = '';
+  gTopTxtPos = 50;
+  gBottomTxtPos = 460;
+  gCenterTxtPos = 265;
+  gNumOfLines = 1;
+  gAddedThirdLine = false;
+  gLastLine = 0;
+  renderMeme();
 }
 
 function getImages() {
-    return gImgs;
+  return gImgs;
 }
 
-
-
 function renderMeme() {
-    // const meme = getMeme()
-    var renderedImg = new Image();
-    renderedImg.onload = function () {
-        gCtx.drawImage(renderedImg, 0, 0);
-        gCtx.transform(1, 0, 0, -1, 0, gElCanvas)
-        // meme.lines.forEach(line => {
-        //     drawLine(line)
-        // }
-        gCtx.fillStyle = `${gMeme.lines[0].color}`;
-        gCtx.textAlign = `${gMeme.lines[0].align}`;
-        gCtx.font = `bold ${gMeme.lines[0].size}px ${gMeme.lines[0].font}`;
-        gCtx.fillText(gMeme.lines[0].txt, gElCanvas.width / 2, gTopTxtPos);
-        gCtx.strokeText(gMeme.lines[0].txt, gElCanvas.width / 2, gTopTxtPos);
-        gCtx.fillStyle = `${gMeme.lines[1].color}`;
-        gCtx.textAlign = `${gMeme.lines[1].align}`;
-        gCtx.font = `bold ${gMeme.lines[1].size}px ${gMeme.lines[1].font}`;
-        gCtx.fillText(gMeme.lines[1].txt, gElCanvas.width / 2, gBottomTxtPos);
-        gCtx.strokeText(gMeme.lines[1].txt, gElCanvas.width / 2, gBottomTxtPos);
-        gCtx.fillStyle = `${gMeme.lines[2].color}`;
-        gCtx.textAlign = `${gMeme.lines[2].align}`;
-        gCtx.font = `bold ${gMeme.lines[2].size}px ${gMeme.lines[2].font}`;
-        gCtx.fillText(gMeme.lines[2].txt, gElCanvas.width / 2, gCenterTxtPos);
-        gCtx.strokeText(gMeme.lines[2].txt, gElCanvas.width / 2, gCenterTxtPos);
-    }
-    renderedImg.src = gCurrImgUrl;
+  // const meme = getMeme()
+  var renderedImg = new Image();
+  renderedImg.onload = function () {
+    gCtx.drawImage(renderedImg, 0, 0);
+    gCtx.transform(1, 0, 0, -1, 0, gElCanvas);
+    // meme.lines.forEach(line => {
+    //     drawLine(line)
+    // }
+    gCtx.fillStyle = `${gMeme.lines[0].color}`;
+    gCtx.textAlign = `${gMeme.lines[0].align}`;
+    setHorizontalPos(gCtx.textAlign);
+    gCtx.font = `bold ${gMeme.lines[0].size}px ${gMeme.lines[0].font}`;
+    gCtx.fillText(gMeme.lines[0].txt, gHorizontalPos, gTopTxtPos);
+    gCtx.strokeText(gMeme.lines[0].txt, gHorizontalPos, gTopTxtPos);
+    gCtx.fillStyle = `${gMeme.lines[1].color}`;
+    gCtx.textAlign = `${gMeme.lines[1].align}`;
+    setHorizontalPos(gCtx.textAlign);
+    gCtx.font = `bold ${gMeme.lines[1].size}px ${gMeme.lines[1].font}`;
+    gCtx.fillText(gMeme.lines[1].txt, gHorizontalPos, gBottomTxtPos);
+    gCtx.strokeText(gMeme.lines[1].txt, gHorizontalPos, gBottomTxtPos);
+    gCtx.fillStyle = `${gMeme.lines[2].color}`;
+    gCtx.textAlign = `${gMeme.lines[2].align}`;
+    setHorizontalPos(gCtx.textAlign);
+    gCtx.font = `bold ${gMeme.lines[2].size}px ${gMeme.lines[2].font}`;
+    gCtx.fillText(gMeme.lines[2].txt, gHorizontalPos, gCenterTxtPos);
+    gCtx.strokeText(gMeme.lines[2].txt, gHorizontalPos, gCenterTxtPos);
+  };
+  renderedImg.src = gCurrImgUrl;
+}
+
+function setHorizontalPos(pos) {
+  if (pos === 'left') gHorizontalPos = 25;
+  if (pos === 'center') gHorizontalPos = 250;
+  if (pos === 'right') gHorizontalPos = 475;
 }
 
 function onChangeTxt(e) {
-    var text = e.target.value;
-    gCurrMemeLines.txt = text
-    renderMeme();
-    var elTxt = document.querySelector('.meme-text-input');
-    elTxt.value = ''
+  var elTxt = document.querySelector('.meme-text-input');
+  var text = elTxt.value;
+  if (text === '') {
+    elTxt.focus();
+    return;
+  }
+  gMeme.lines[gMeme.selectedLineIdx].txt = text;
+  console.log(gMeme.lines);
+  renderMeme();
+  elTxt.value = '';
+  elTxt.focus();
 }
 
 function onChangeTxtColor() {
-    const EL_COLOR_SELECTOR_VAL = document.querySelector('[name="txt-color"]').value;
-    gCurrMemeLines.color = EL_COLOR_SELECTOR_VAL;
-    renderMeme();
+  const EL_COLOR_SELECTOR_VAL =
+    document.querySelector('[name="txt-color"]').value;
+  gMeme.lines[gMeme.selectedLineIdx].color = EL_COLOR_SELECTOR_VAL;
+  renderMeme();
 }
 
 function onChangeTxtSize(size) {
-    if (size === 'up') gCurrMemeLines.size += 3;
-    else gCurrMemeLines.size -= 3;
-    renderMeme();
+  if (size === 'up') gMeme.lines[gMeme.selectedLineIdx].size += 3;
+  else gMeme.lines[gMeme.selectedLineIdx].size -= 3;
+  renderMeme();
+}
+
+function countNumOfLines() {
+  console.log(gMeme.lines);
+  console.log(gMeme.lines[0].txt);
+  console.log(gMeme.lines[1].txt);
+  console.log(gMeme.lines[2].txt);
+  if (
+    gMeme.lines[0].txt === '' &&
+    gMeme.lines[1].txt === '' &&
+    gMeme.lines[2].txt === ''
+  ) {
+    console.log('if rishon');
+    gNumOfLines = 1;
+    return;
+  }
+  if (
+    (gMeme.lines[0].txt === '' && gMeme.lines[1].txt === '') ||
+    (gMeme.lines[1].txt === '' && gMeme.lines[2].txt === '') ||
+    (gMeme.lines[2].txt === '' && gMeme.lines[0].txt === '')
+  ) {
+    console.log('if sheni');
+    gNumOfLines = 2;
+    return;
+  }
+  console.log('shalosh');
+  gNumOfLines = 3;
 }
 
 function onSwitchLine() {
-    if (gNumOfLines === 0) return;
-    if (gNumOfLines === 2) {
-        if (gLastLine === 0) {
-            gCurrMemeLines = gMeme.lines[1];
-            gLastLine = 1;
-        }
-        else {
-            gCurrMemeLines = gMeme.lines[0];
-            gLastLine = 0;
-
-        }
+  if (gNumOfLines === 1) {
+    console.log('gNumOfLines===1');
+    gLastLine = gMeme.selectedLineIdx;
+    return;
+  }
+  countNumOfLines();
+  if (gNumOfLines === 2) {
+    console.log('gNumOfLines===2');
+    if (!gAddedThirdLine) {
+      console.log('gAddedThirdLine === false');
+      console.log(gLastLine);
+      if (gLastLine === 0) {
+        gMeme.selectedLineIdx = 1;
+        gLastLine = 1;
+      } else {
+        gMeme.selectedLineIdx = 0;
+        gLastLine = 0;
+      }
     }
-    else {
-        if (!gAddedThirdLine) {
-            if (gCurrMemeLines === gMeme.lines[0]) gLastLine = 0;
-            else gLastLine = 1;
-            gCurrMemeLines = gMeme.lines[2];
-            gAddedThirdLine = true;
-
-        }
-        else {
-            if (gCurrMemeLines === gMeme.lines[0] || gCurrMemeLines === gMeme.lines[1]) gCurrMemeLines = gMeme.lines[2];
-            else if (gLastLine === 0) {
-                gCurrMemeLines = gMeme.lines[1]
-                gLastLine = 1;
-            }
-            else {
-                gCurrMemeLines = gMeme.lines[0]
-                gLastLine = 0;
-            }
-        }
+  } else {
+    console.log('gNumOfLines===3');
+    if (!gAddedThirdLine) {
+      console.log('hey');
+      if (gMeme.selectedLineIdx === 0) gLastLine = 0;
+      else gLastLine = 1;
+      gMeme.selectedLineIdx = 2;
+      gAddedThirdLine = true;
+    } else {
+      if (gMeme.selectedLineIdx === 0 || gMeme.selectedLineIdx === 1)
+        gMeme.selectedLineIdx = 2;
+      else if (gLastLine === 0) {
+        gMeme.selectedLineIdx = 1;
+        gLastLine = 1;
+      } else {
+        gMeme.selectedLineIdx = 0;
+        gLastLine = 0;
+      }
     }
+  }
+  console.log('gMeme.selectedLineIdx', gMeme.selectedLineIdx);
+  //   console.log('gCurrMemeLines', gCurrMemeLines);
+  //   console.log('gLastLine', gLastLine);
 }
 
 function onRaiseText() {
-    if (gCurrMemeLines === gMeme.lines[0]) gTopTxtPos -= 3;
-    else if (gCurrMemeLines === gMeme.lines[1]) gBottomTxtPos -= 3;
-    else gCenterTxtPos -= 3;
-    renderMeme();
+  if (gMeme.selectedLineIdx === 0) gTopTxtPos -= 3;
+  else if (gMeme.selectedLineIdx === 1) gBottomTxtPos -= 3;
+  else gCenterTxtPos -= 3;
+  renderMeme();
 }
 
 function onLowerText() {
-    if (gCurrMemeLines === gMeme.lines[0]) gTopTxtPos += 3;
-    else if (gCurrMemeLines === gMeme.lines[1]) gBottomTxtPos += 3;
-    else gCenterTxtPos += 3;
-    renderMeme();
+  if (gMeme.selectedLineIdx === 0) gTopTxtPos += 3;
+  else if (gMeme.selectedLineIdx === 1) gBottomTxtPos += 3;
+  else gCenterTxtPos += 3;
+  renderMeme();
 }
 
 function onSetFont(font) {
-    if (gCurrMemeLines === gMeme.lines[0]) gMeme.lines[0].font = font;
-    else gMeme.lines[1].font = font;
-    renderMeme();
+  if (gMeme.selectedLineIdx === 0) gMeme.lines[0].font = font;
+  if (gMeme.selectedLineIdx === 1) gMeme.lines[1].font = font;
+  if (gMeme.selectedLineIdx === 2) gMeme.lines[2].font = font;
+  renderMeme();
 }
 
 function onTextAlign(align) {
-    if (gCurrMemeLines === gMeme.lines[0]) gMeme.lines[0].align = align;
-    else gMeme.lines[1].align = align;
-    renderMeme();
+  if (gMeme.selectedLineIdx === 0) gMeme.lines[0].align = align;
+  if (gMeme.selectedLineIdx === 1) gMeme.lines[1].align = align;
+  if (gMeme.selectedLineIdx === 2) gMeme.lines[2].align = align;
+  renderMeme();
 }
 
 function downloadMeme(link) {
-    const DATA = gElCanvas.toDataURL();
-    link.href = DATA;
-    link.download = "YourMeme.jpg";
+  const DATA = gElCanvas.toDataURL();
+  link.href = DATA;
+  link.download = 'YourMeme.jpg';
 }
 
 function onAddLine() {
-    switch (gNumOfLines) {
-        case 0:
-            gNumOfLines = 1;
-            break;
-        case 1:
-            gNumOfLines = 2;
-            break;
-        case 2:
-            gNumOfLines = 3;
-            break;
-        case 3:
-            return;
-    }
-    onSwitchLine();
+  switch (gNumOfLines) {
+    case 1:
+      gNumOfLines = 2;
+      break;
+    case 2:
+      gNumOfLines = 3;
+      gAddedThirdLine = true;
+      break;
+    case 3:
+      return;
+  }
+  onSwitchLine();
 }
 
-function linesCountForRemove() {
-    switch (gNumOfLines) {
-        case 3:
-            gNumOfLines = 2;
-            break;
-        case 2:
-            gNumOfLines = 1;
-            break;
-        case 1:
-            gNumOfLines = 0;
-            break;
-        case 0:
-            return;
+function checkIfSingleLine() {
+  if (gMeme.selectedLineIdx === 0) {
+    if (gMeme.lines[1].txt === '' && gMeme.lines[2].txt === '') {
+      console.log(true);
+      return true;
+    } else {
+      console.log(false);
+      return false;
     }
+  }
+  if (gMeme.selectedLineIdx === 1) {
+    if (gMeme.lines[0].txt === '' && gMeme.lines[2].txt === '') {
+      console.log(true);
+      return true;
+    } else {
+      console.log(false);
+      return false;
+    }
+  }
+  if (gMeme.selectedLineIdx === 2) {
+    if (gMeme.lines[0].txt === '' && gMeme.lines[1].txt === '') {
+      console.log(true);
+      return true;
+    } else {
+      console.log(false);
+      return false;
+    }
+  }
+}
+
+// function onAddLine() {
+
+// }
+
+function linesCountForRemove() {
+  switch (gNumOfLines) {
+    case 3:
+      gNumOfLines = 2;
+      gAddedThirdLine = false;
+      break;
+    case 2:
+      gNumOfLines = 1;
+      break;
+    case 1:
+      return;
+  }
 }
 
 function onRemoveLine() {
-    if (gCurrMemeLines === gMeme.lines[0]) {
-        gMeme.lines[0].txt = '';
-        linesCountForRemove();
+  console.log(gMeme.selectedLineIdx);
+  if (gMeme.selectedLineIdx === 0) {
+    console.log('im here0');
+    gMeme.lines[0].txt = '';
+    if (checkIfSingleLine()) {
+      linesCountForRemove();
+      onSwitchLine();
     }
-    else if (gCurrMemeLines === gMeme.lines[1]) {
-        gMeme.lines[1].txt = '';
-        gNumOfLines = 2;
+  }
+  if (gMeme.selectedLineIdx === 1) {
+    console.log('im here1');
+    gMeme.lines[1].txt = '';
+    if (checkIfSingleLine()) {
+      console.log('im here1 inner');
+      linesCountForRemove();
+      onSwitchLine();
     }
-    else {
-        gMeme.lines[2].txt = '';
-        gNumOfLines = 2;
-        gAddedThirdLine = false;
-        if (gLastLine === 0) gCurrMemeLines === gMeme.lines[1];
-        else gCurrMemeLines === gMeme.lines[0];
+  }
+  if (gMeme.selectedLineIdx === 2) {
+    console.log('im here2');
+    gMeme.lines[2].txt = '';
+    if (checkIfSingleLine()) {
+      linesCountForRemove();
+      onSwitchLine();
     }
-    renderMeme();
+  }
+  renderMeme();
 }
 
-var text = document.getElementById("text-change");
-text.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {
-        onChangeTxt(e);
-    }
+var text = document.getElementById('text-change');
+text.addEventListener('keydown', function (e) {
+  if (e.code === 'Enter') {
+    onChangeTxt(e);
+  }
 });
-
